a6f418c0da4393de86376f8df2715b959c0d1ccd
Aguacate20 <sebas.galindezaco@gmail.com>
2025-07-30 11:07:47 -0500

    Tamaño y posición de las imagenes

--- app.py
+++ app.py
@@ -122,12 +122,14 @@ st.markdown("""
         color: white !important;
         cursor: not-allowed !important;
     }
-    .image-container img {
-        width: 100px;
-        height: auto;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
+    .stImage > img {
+    width: 350px; /* Mantén o ajusta el tamaño */
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-left: 20px; /* Aumenta este valor para mover la imagen en col1 (IA) a la izquierda */
+    margin-right: 20px; /* Aumenta este valor para mover la imagen en col3 (Humano) a la derecha */
     }
     </style>
 """, unsafe_allow_html=True)
@@ -253,12 +255,12 @@ def main():
         with col1:
             response = requests.get("https://raw.githubusercontent.com/SebastianFullStack/images/main/IA.png")
             img_ai = Image.open(BytesIO(response.content))
-            st.image(img_ai, caption=" ", use_column_width=False, width=200)
+            st.image(img_ai, caption=" ", use_column_width=False, width=300)
             st.markdown("<span class='slider-label'>IA</span>", unsafe_allow_html=True)
         with col3:
             response = requests.get("https://raw.githubusercontent.com/SebastianFullStack/images/main/Humano.png")
             img_human = Image.open(BytesIO(response.content))
-            st.image(img_human, caption=" ", use_column_width=False, width=200)
+            st.image(img_human, caption=" ", use_column_width=False, width=300)
             st.markdown("<span class='slider-label'>Humano</span>", unsafe_allow_html=True)
         
         with col2:
