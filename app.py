3a35a082d66c10bc2906028ea3283d499e2eeaf6
Aguacate20 <sebas.galindezaco@gmail.com>
2025-07-30 14:17:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ st.markdown("""
         div[data-testid="column"]:nth-child(3) .slider-label { /* Etiqueta Humano */
             position: relative; /* ¡CLAVE! */
             right: 220px !important; /* Mueve horizontalmente. Ajusta para alinear con la imagen. */
-            top: 5px !important; /* Mueve verticalmente. */
+            top: 100px !important; /* Mueve verticalmente. */
         }
 
         /* Bloque para modificar la posición de la situación */
